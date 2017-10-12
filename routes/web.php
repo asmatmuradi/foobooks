2f3179a49655d4286b31f23167c54481b1f2fc74
AsmatUllah Muradi <Asmat@AsmatUllahs-MacBook-Pro.local>
2017-10-11 23:07:44 -0400

    week6 additions

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController');
+
+Route::get('/book', 'BookController@index');
+Route::get('/book/{title}','BookController@show');
